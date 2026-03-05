0bc287bd58f60aa0298cbe39a220956f52151392
Rosiberto <rosibertosantos@gmail.com>
2026-03-05 14:17:05 -0300

    update tests

--- tests/eplBuilder.test.js
+++ tests/eplBuilder.test.js
@@ -1,27 +1,21 @@
-import { buildEPLFromNodes } 
-from '../../editor/static/js/eplBuilder.js';
+const { buildEPLFromNodes } = require('../../editor/static/js/eplBuilder.js');
 
 test('gera SELECT simples', () => {
 
   const nodes = {
-
     1: {
       name: 'RULE',
-      data: { rule: 'temperature_rule' }
+      data: { rule: 'temp_rule' }
     },
-
     2: {
       name: 'SELECT',
       data: { select: 'temperature' }
     }
-
   };
 
   const result = buildEPLFromNodes(nodes);
 
-  expect(result.rule_name).toBe('temperature_rule');
-
-  expect(result.epl)
-    .toContain('SELECT *, temperature FROM iotEvent');
+  expect(result.rule_name).toBe('temp_rule');
+  expect(result.epl).toContain('SELECT');
 
 });
